e40d1bbdd9db7b77aead73972b4ad8f00fbc0c47
McKenna Leichty <85258611+mckleichty@users.noreply.github.com>
2025-12-03 20:09:13 -0500

    Update util.py

--- util.py
+++ util.py
@@ -728,9 +728,12 @@ def gaussian_fitter_new(peak_wavelength, fit_width, wavelengths, flux, flux_err,
     # -------------------------
     if plot:
         fig = make_subplots(rows=2, cols=1, shared_xaxes=True,
-                            row_heights=[0.7, 0.3],
+                            row_heights=[0.7, 0.5],
                             vertical_spacing=0.2,
                             subplot_titles=[f"Fit of the {title} line", "O-C Residuals"])
+
+        # Make subplot titles black
+        fig.update_annotations(font=dict(color='black'))
 
         # full spectrum
         fig.add_trace(go.Scatter(
